08952293524427117248e192b8f92adc8e3dc5a6
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2020-05-06 16:29:08 +0300

    Creating a Spring Boot Service

---
+++ src/main/java/com/accenture/training_task/TrainingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.training_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/training_task/TrainingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.training_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
