1d09c30e733ff75efcad2fa7a46b4db0b235d4b8
inna.tisina <innapbm@gmail.com>
2020-05-14 14:05:45 +0300

    .

--- src/test/java/com/accenture/training_task/TrainingTaskApplicationTests.java
+++ src/test/java/com/accenture/training_task/TrainingTaskApplicationTests.java
@@ -1,15 +1,19 @@
 package com.accenture.training_task;
 
 import com.accenture.training_task.controller.FlightsController;
+import com.accenture.training_task.flightAPI.APIservice;
 import com.accenture.training_task.model.FlightData;
+import com.accenture.training_task.repository.DataRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.File;
@@ -19,15 +23,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-class TrainingTaskApplicationTests {
-
-    @LocalServerPort
-    private int port;
+public class TrainingTaskApplicationTests {
 
     @Autowired
     private FlightsController controller;
@@ -35,38 +37,65 @@ class TrainingTaskApplicationTests {
     @Autowired
     private TestRestTemplate restTemplate;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private DataRepository dataRepository;
+
+    @Autowired
+    private APIservice apIservice;
 
     @Test
     public void contextLoads() throws Exception {
         assertThat(controller).isNotNull();
     }
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
     public void testFlightsPage() throws Exception {
-        File flights = new ClassPathResource("flights.html").getFile();
+        File flights = new ClassPathResource("templates/flights.html").getFile();
         String html = new String((Files.readAllBytes(flights.toPath())));
+
+        Thread.sleep(2000);
 
         this.mockMvc.perform(get("/api/flights"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(html))
+                .andExpect(view().name("flights"))
+                .andExpect(model().attributeExists("flights"))
                 .andDo(print());
     }
 
     @Test
-    public void addFlightToFavourites() throws Exception {
+    public void addFlightToJPAFavorites() throws Exception {
 
-		FlightData flightData = new FlightData("Riga Airport", "Copenhagen Kastrup airport", "Norwegian", "F3456");
-        mockMvc.perform(post("/api/jpa/addFavorite/{flightNumber}", flightData.getFlightNumber())
-				.content(objectMapper.writeValueAsString(flightData)))
-				.andExpect(status().isOk());
+        FlightData flightData = new FlightData("Riga Airport",
+                "Copenhagen Kastrup airport", "Norwegian", String.valueOf(Math.random()));
+
+        mockMvc.perform(
+                post("/api/jpa/favorites/add/{flightNumber}", flightData.getFlightNumber())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(flightData)))
+                .andExpect(status().isCreated());
     }
 
+    @Test
+    public void addFlightsToFavorites() throws Exception {
 
+        FlightData flightData = new FlightData("Riga Airport",
+                "Copenhagen Kastrup airport", "Norwegian", String.valueOf(Math.random()));
+        String URL = "/api/favorites/add?flightNumber=" + flightData.getFlightNumber();
+
+        mockMvc.perform(
+                post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(flightData)))
+                .andExpect(status().isCreated());
+    }
 
 }
+
+
 
